10a5f014a8ed3c1a955a0e226ee4e01232eff84c
Cris <42557012+cristianescs@users.noreply.github.com>
2021-03-30 16:34:49 -0300

    commit inicial

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        900:{
+            items:4
+        },
+        1200:{
+            items:7
+        }
+    }
+})
